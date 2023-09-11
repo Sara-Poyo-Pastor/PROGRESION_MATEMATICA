56cd87f7a69da805ce76091e7e639fdea27aa010
Sara-Poyo-Pastor <txara88@hotmail.com>
2023-09-11 14:25:40 +0200

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css"
+
+
+
+const Footer = () => {
+    return (
+        <> 
+            <div className="footerContainer">
+                <footer className="footer"> &#169; PROGRESIÓN MATEMÁTICA. Todos los derechos reservados</footer>
+            </div>  
+        </>
+    )
+}
+
+export default Footer
