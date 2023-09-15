f233fff2a5e9be9ccc6a4b67bbabd77470e7c974
Sara-Poyo-Pastor <txara88@hotmail.com>
2023-09-15 11:05:12 +0200

    login view

--- src/pages/loginView/LoginView.jsx
+++ src/pages/loginView/LoginView.jsx
@@ -1,20 +1,63 @@
-import React from 'react'
+import "./LoginView.css";
+import { useState } from 'react';
+
+
 
 function LoginView() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para enviar los datos de inicio de sesión al servidor
+    // Por ejemplo, puedes usar fetch o axios para hacer una solicitud a tu API
+    console.log('Datos de inicio de sesión:', { email, password });
+  };
+
+  const handleCancel = () => {
+    // Agrega la lógica para cancelar el inicio de sesión
+    console.log('Inicio de sesión cancelado');
+  };
+
   return (
-    <div className="loginPage">
-
-<h1 className ="loginPage-title">LOG IN</h1>
-
-<form>
-
-<input type ="text" name= "username"></input>
-
-<input type= "password" name= "password"/>
-
-</form>
-
-</div>)
+    <div>
+      <h2 className="loginTitle">Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Aceptar</button>
+          <button type="button" onClick={handleCancel}>Cancelar</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default LoginView
+export default LoginView;
+
+
